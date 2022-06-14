b668dd6a83fce8fa246bd31e4fd594c64c2d2072
Student: Aya Aya <94848837+ayamuthanna@users.noreply.github.com>
2022-06-14 06:16:35 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+
+/*
+    String str1 = "wow";
+    String str2 = "xow";
+
+    System.out.println(str1.compareTo(str2));
+
+    String[] arr = {str1, str2};
+
+      String temp = arr[1];
+      arr[1] = arr[0];
+      arr[0] = temp;
+      
+
+    System.out.println(arr[0]);
+    System.out.println(arr[1]);
+
+
+    String[] helloWorld = {"Hello", "World"};
+    System.out.println(helloWorld);
+    System.out.println(Arrays.toString(helloWorld));
+*/
+    ArrayList<Integer> elementsArray = new ArrayList<> ();
+
+  elementsArray.add(21);
+  elementsArray.add(17);
+  elementsArray.add(60);
+  elementsArray.add(20);
+  elementsArray.add(56);
+  elementsArray.add(12);
+
+
+        System.out.println(elementsArray);
+  }
+}
